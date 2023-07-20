78506ae080d3ded109b07cdbc1cd59aa82e0c20f
Administrator <kristofer.souza2027@hotmail.com>
2023-07-19 16:32:39 -0800

    Site em desenvolvimento...

--- processo-seletivo-crud/src/app/components/pipes/pipes.component.ts
+++ processo-seletivo-crud/src/app/components/pipes/pipes.component.ts
@@ -6,6 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatarDataPipe implements PipeTransform {
   transform(value: string): string {
+    if (value === '1899-11-30T08:00:00.000Z') {
+      return '0000-00-00';
+    }
+
     const dataObj = new Date(value);
     const dia = dataObj.getDate().toString().padStart(2, '0');
     const mes = (dataObj.getMonth() + 1).toString().padStart(2, '0');
